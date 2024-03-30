db5a001085b3c66729f53db8b523063f2407228a
Mooner92 <54096383+mooner92@users.noreply.github.com>
2024-03-31 00:23:36 +0900

    add

---
+++ 달팽이삼각형.py
@@ -0,0 +1,34 @@
+def solution(n):
+    st = [[0 for _ in range(1000)] for _ in range(1000)]
+    answer = []
+    t = 1
+    for i in range(n):
+        u = i // 3
+        if i % 3 == 0:
+            for j in range(0, n - u * 3):
+                st[u * 2 + j][u] = t
+                # print("{0} {1} {2}".format(u*2+j ,u,t))
+                t += 1
+
+        elif i % 3 == 1:
+            for j in range(0, n - u * 3 - 1):
+                st[n - u - 1][u + 1 + j] = t
+                # print("{0} {1} {2}".format(n-u-1,u+1+j,t))
+                t += 1
+
+        elif i % 3 == 2:
+            for j in range(0, n - u * 3 - 2):
+                st[n - 2 - u - j][n - 2 - (u * 2) - j] = t
+                # print("{0} {1} {2}".format(n-2-u-j ,n-2-(u*2)-j,t))
+                t += 1
+
+        # print("\n\n")
+    for i in range(n):
+        for j in range(n):
+            # print(st[i][j],end=' ')
+            if st[i][j] != 0:
+                answer.append(st[i][j])
+                # print("{0} {1} {2}".format(i,j,st[i][j]))
+        # print()
+
+    return answer
