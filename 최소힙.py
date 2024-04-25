0bbec99df3abda8c928e59e41da2fcdbbfc1063c
mooner92 <chlaudgjs0989@gmail.com>
2024-04-25 22:28:20 +0900

    add

---
+++ 최소힙.py
@@ -0,0 +1,16 @@
+
+import sys
+import heapq
+
+a = int(sys.stdin.readline())
+max_heap = []
+
+for i in range(a):
+    b = int(sys.stdin.readline())
+    if b == 0:
+        if not max_heap:
+            print(0)
+        else:
+            print(heapq.heappop(max_heap))
+    else:
+        heapq.heappush(max_heap, b)
